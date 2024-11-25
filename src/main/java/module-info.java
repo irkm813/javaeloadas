5c9e84cb50e0596b27986a13e1eca5104ae54386
Ferenc Szabo <irkm813@gmail.com>
2024-11-26 00:37:13 +0100

    Az oanda api jól megy, ki tudom iratni, de a formátum hányadék, sikerült az mnb-s class-okat is hozzáadni ehhez a szerencsétlen projekthez, valamint megoldottam, hogy minden dependency-t maven-ből töltsön be. Még pár ilyen este és felkötöm magam.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,24 +1,26 @@
 module com.example.javaeloadasbeadando {
     requires javafx.fxml;
     requires javafx.controls;
+    requires jakarta.xml.ws;
     requires gson;
     requires httpcore;
     requires httpclient;
     requires java.sql;
 
-    //opens com.oanda.v20;
-    //opens com.oanda.v20.account;
-    //opens com.oanda.v20.pricing;
-    //opens com.oanda.v20.pricing_common;
-    //opens com.oanda.v20.order;
-    //opens com.oanda.v20.instrument;
-    //opens com.oanda.v20.transaction;
-    //opens com.oanda.v20.trade;
-    //exports com.oanda.v20.primitives;
-    //exports com.oanda.v20.transaction;
-    //exports com.oanda.v20.pricing_common;
-    //exports com.oanda.v20.order;
-    //exports com.oanda.v20.trade;
+    opens com.oanda.v20;
+    opens com.oanda.v20.account;
+    opens com.oanda.v20.pricing;
+    opens com.oanda.v20.pricing_common;
+    opens com.oanda.v20.order;
+    opens com.oanda.v20.instrument;
+    opens com.oanda.v20.transaction;
+    opens com.oanda.v20.trade;
+
+    exports com.oanda.v20.primitives;
+    exports com.oanda.v20.transaction;
+    exports com.oanda.v20.pricing_common;
+    exports com.oanda.v20.order;
+    exports com.oanda.v20.trade;
 
     opens com.example.javaeloadasbeadando.lotto_query to javafx.base;
     opens com.example.javaeloadasbeadando to javafx.fxml;
